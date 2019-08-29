3be13534aea22cd8937d01f99a66cd3d8c67d66f
Matthew <Allenm40@my.erau.edu>
2019-08-29 13:45:32 -0400

    Add project files.

---
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //GET action method
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
